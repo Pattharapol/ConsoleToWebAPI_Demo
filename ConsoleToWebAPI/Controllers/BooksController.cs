c7e6fa2e06b8a16ef4b6cfb634e97280dc763629
unknown <tjtik83@gmail.com>
2022-02-06 06:52:20 +0700

    recieve by int and string

---
+++ ConsoleToWebAPI/Controllers/BooksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BooksController : ControllerBase
+    {
+        [Route("{id:int:min(10):max(100)}")]
+        public string GetByID(int id)
+        {
+            return $"Hello Int {id}";
+        }
+
+        [Route("{id}:minlength(5)")]
+        public string GetByID(string id)
+        {
+            return $"Hello String {id}";
+        }
+    }
+}
