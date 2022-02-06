8839d5f25c69b237a53169e8a18c2f14ac77eef8
Pattharapol Lakboon <53226136+Pattharapol@users.noreply.github.com>
2022-02-06 15:04:49 +0700

    also another comment

---
+++ ConsoleToWebAPI/Controllers/CountriesController.cs
@@ -0,0 +1,54 @@
+﻿using ConsoleToWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountriesController : ControllerBase
+    {
+        // pass values as form-data
+        //[BindProperty]
+        //[BindProperty(supportsGet = true)]
+
+        //public CountryModel country { get; set; }
+
+        //public string Name { get; set; }
+
+        //public int Population { get; set; }
+
+        //public int Area { get; set; }
+
+        // pass data from url like ~/api/countries?name=XXX&area=xxx
+        //[HttpPost("{name}/{area}")]
+        //public IActionResult AddCountry(string name, int area)
+        //{
+        //    return Ok($"Name = {name}, Area = {area}");
+        //    //return Ok($"Name = {this.country.Name}, Population = {this.country.Population}, Area = {this.country.Area}");
+        //}
+
+        //[HttpPost("")]
+        //public IActionResult AddCountry(CountryModel country)
+        //{
+        //    return Ok($"Name = {country.Name}, Population = {country.Population}, Area = {country.Area}");
+        //    //return Ok($"Name = {this.country.Name}, Population = {this.country.Population}, Area = {this.country.Area}");
+        //}
+
+        //[HttpPost("")]
+        //public IActionResult AddCountry([FromQuery] CountryModel model)
+        //{
+        //    return Ok($"Name = {model.Name}");
+        //    //return Ok($"Name = {country.Name}, Population = {country.Population}, Area = {country.Area}");
+        //    //return Ok($"Name = {this.country.Name}, Population = {this.country.Population}, Area = {this.country.Area}");
+        //}
+
+        [HttpPost("{name}/{population}/{area}")]
+        public IActionResult AddCountry([FromRoute] CountryModel model)
+        {
+            return Ok($"Name = {model.Name}");
+            //return Ok($"Name = {country.Name}, Population = {country.Population}, Area = {country.Area}");
+            //return Ok($"Name = {this.country.Name}, Population = {this.country.Population}, Area = {this.country.Area}");
+        }
+    }
+}
---
+++ ConsoleToWebAPI/Controllers/EmployeeController.cs
@@ -0,0 +1,60 @@
+﻿using ConsoleToWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ConsoleToWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        //public EmployeeModel GetEmployees()
+        //{
+        //    return new EmployeeModel() { Id = 1, Name = "Employee1" };
+        //}
+
+        [Route("")]
+        public List<EmployeeModel> GetEmployees()
+        {
+            return new List<EmployeeModel>()
+            {
+                new EmployeeModel(){  Id = 1, Name = "Employee1"},
+                new EmployeeModel(){  Id = 2, Name = "Employee2"},
+            };
+        }
+
+        [Route("{id}")]
+        public IActionResult GetEmployees(int id)
+        {
+            // return status
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(
+                 new List<EmployeeModel>()
+           {
+                new EmployeeModel(){  Id = 1, Name = "Employee1"},
+                new EmployeeModel(){  Id = 2, Name = "Employee2"}
+           });
+            }
+        }
+
+        [Route("{id}/basic")]
+        public ActionResult<EmployeeModel> GetEmployeeBasicDetails(int id)
+        {
+            // return status
+            if (id == 0)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return new EmployeeModel() { Id = 1, Name = "Employee1" };
+            }
+        }
+    }
+}
